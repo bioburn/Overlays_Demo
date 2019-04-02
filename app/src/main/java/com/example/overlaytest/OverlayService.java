5911133a63af67f55171660ba7b4b2e058eb3dca
Jackey <jackey@umail.ucsb.edu>
2019-04-02 18:39:57 -0400

    Animated imageview overlays

--- app/src/main/java/com/example/overlaytest/OverlayService.java
+++ app/src/main/java/com/example/overlaytest/OverlayService.java
@@ -12,13 +12,22 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.text.Layout;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -28,6 +37,10 @@ public class OverlayService extends Service {
     Button overlayedButton;
 
     ImageView  myImage;
+
+    FrameLayout floating_layout;
+
+    ImageView SpinningImage;
 
     int width;
     int height;
@@ -87,7 +100,19 @@ public class OverlayService extends Service {
         myImage.setMaxWidth(100);
         myImage.setMaxHeight(100);
 
+        RotateAnimation anim = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim.setInterpolator(new LinearInterpolator());
+        //how many times it rotates
+        anim.setRepeatCount(Animation.ABSOLUTE);
+        //the speed
+        anim.setDuration(2000);
 
+
+
+
+
+        myImage.startAnimation(anim);
+        overlayedButton.startAnimation(anim);
 
         WindowManager.LayoutParams params3 = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -99,8 +124,54 @@ public class OverlayService extends Service {
                 PixelFormat.TRANSPARENT);
 
 
-        wm.addView(myImage,params);
 
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        floating_layout = (FrameLayout) inflater.inflate(R.layout.cutin_layout, null);
+
+
+
+        //wm.addView(myImage,params);
+
+        SpinningImage = floating_layout.findViewById(R.id.imageView2);
+
+        wm.addView(floating_layout,params);
+        SpinningImage.startAnimation(anim);
+
+
+
+        Animation a = new AlphaAnimation(1.00f, 0.00f);
+        a.setDuration(3000);
+        a.setAnimationListener(new Animation.AnimationListener() {
+
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void onAnimationEnd(Animation animation) {
+                SpinningImage.setVisibility(View.GONE);
+
+            }
+        });
+
+        Animation b = new AlphaAnimation(0.00f, 1.00f);
+
+        b.setDuration(1000);
+
+        AnimationSet s = new AnimationSet(false);
+        s.addAnimation(b);
+        s.addAnimation(anim);
+        s.addAnimation(a);
+
+        SpinningImage.startAnimation(s);
+
+        myImage.startAnimation(anim);
+        overlayedButton.startAnimation(anim);
 
         Log.d("onCreate", "end");
     }
@@ -116,11 +187,22 @@ public class OverlayService extends Service {
             overlayedButton = null;
         }
 
-        if(myImage != null)
+        //if(myImage != null)
+        //{
+            //((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(myImage);
+            //myImage = null;
+        //}
+
+
+        if(floating_layout!=null)
         {
-            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(myImage);
-            myImage = null;
+            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(floating_layout);
+            floating_layout = null;
         }
+
+
+
+
     }
 
 
